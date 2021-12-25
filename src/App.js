14a0abdb0b58dbf328fcfd6f9264d46ea7c0e5eb
Nithin M.K <nithinmkurien@gmail.com>
2021-12-25 23:40:30 +0530

    03-a-closer-look-at-child-cmp-reevaluation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
