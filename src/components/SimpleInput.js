2b9934dad0e4021f64a89e12cb33f3bfca9f0bbb
Nithin M.K <nithinmkurien@gmail.com>
2022-03-14 13:19:59 +0530

    07-managing-the-overall-form-validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -7,11 +7,17 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
-  const nameInputBlurHandler = event => {
+  const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
   };
 
@@ -51,7 +57,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
