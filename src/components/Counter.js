297dfa4ad742d7148e576de75b96067207f134fd
Nithin M.K <nithinmkurien@gmail.com>
2022-05-23 12:29:33 +0530

    06-attaching-payloads-to-actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,14 @@ import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter);
+  const counter = useSelector((state) => state.counter);
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 10 });
   };
 
   const decrementHandler = () => {
@@ -22,6 +26,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
