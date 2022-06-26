c7d3fa0e467be14c8d9b924157a1f4213ce09cce
Nithin M.K <nithinmkurien@gmail.com>
2022-06-26 19:26:11 +0530

    09-time-to-practice-starting-code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
