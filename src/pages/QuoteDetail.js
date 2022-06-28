713986970e71367d58eb7b80ae4f7c29dfe65dec
Nithin M.K <nithinmkurien@gmail.com>
2022-06-28 11:53:13 +0530

    11-practicing-nested-routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,7 @@
 import { Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Route } from 'react-router-dom';
+
+import Comments from '../components/comments/Comments';
 
 const QuoteDetail = () => {
   const params = useParams();
@@ -8,6 +10,9 @@ const QuoteDetail = () => {
     <Fragment>
       <h1>Quote Detail Page</h1>
       <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
