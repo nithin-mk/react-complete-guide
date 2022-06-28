cbd4c4fe8267303dc3075f84d8973481461f8f65
Nithin M.K <nithinmkurien@gmail.com>
2022-06-28 11:13:25 +0530

    10-practice-redirecting-and-extracting-params

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes' />
+      </Route>
+      <Route path='/quotes' exact>
+        <AllQuotes />
+      </Route>
+      <Route path='/quotes/:quoteId'>
+        <QuoteDetail />
+      </Route>
+      <Route path='/new-quote'>
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
