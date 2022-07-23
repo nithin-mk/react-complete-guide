65b871f61d9ed5604d2f1c3a9df0ed813d8e7473
Nithin M.K <nithinmkurien@gmail.com>
2022-07-23 19:36:31 +0530

    09-finished

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -62,7 +62,10 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        authCtx.login(data.idToken);
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        authCtx.login(data.idToken, expirationTime.toISOString());
         history.replace('/');
       })
       .catch((err) => {
