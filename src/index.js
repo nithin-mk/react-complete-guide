58ff8f9ad7606aadb3b36e180cf1c9b19c067d3d
Nithin M.K <nithinmkurien@gmail.com>
2022-08-30 22:14:53 +0530

    01-starting-project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import productReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  shop: productReducer,
+});
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-registerServiceWorker();
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
