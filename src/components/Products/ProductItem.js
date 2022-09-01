c1d94e6aa9f6a2f88979ab577f8343b9768b7f61
Nithin M.K <nithinmkurien@gmail.com>
2022-09-01 10:24:01 +0530

    02-context-api-summary

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useContext } from 'react';
 
 import Card from '../UI/Card';
+import { ProductsContext } from '../../context/products-context';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
 
 const ProductItem = props => {
-  const dispatch = useDispatch();
+
+  const toggleFav = useContext(ProductsContext).toggleFav;
 
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    toggleFav(props.id);
   };
 
   return (
