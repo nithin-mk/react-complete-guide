de1dcc1304b88f7e2a51140962ed5aa74bf5cd7d
Nithin M.K <nithinmkurien@gmail.com>
2022-09-01 13:25:55 +0530

    04-finishing-the-store-hook

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,15 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import Card from '../UI/Card';
-import { ProductsContext } from '../../context/products-context';
+import { useStore } from '../../hooks-store/store';
 import './ProductItem.css';
 
 const ProductItem = props => {
-
-  const toggleFav = useContext(ProductsContext).toggleFav;
+  const dispatch = useStore()[1];
 
   const toggleFavHandler = () => {
-    toggleFav(props.id);
+    // toggleFav(props.id);
+    dispatch('TOGGLE_FAV', props.id);
   };
 
   return (
