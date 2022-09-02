66d01f86a8382220b26cc3a023522567d8e7eb2b
Nithin M.K <nithinmkurien@gmail.com>
2022-09-02 11:57:36 +0530

    05-finished

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -4,8 +4,9 @@ import Card from '../UI/Card';
 import { useStore } from '../../hooks-store/store';
 import './ProductItem.css';
 
-const ProductItem = props => {
-  const dispatch = useStore()[1];
+const ProductItem = React.memo(props => {
+  console.log('RENDERING');
+  const dispatch = useStore(false)[1];
 
   const toggleFavHandler = () => {
     // toggleFav(props.id);
@@ -26,6 +27,6 @@ const ProductItem = props => {
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
