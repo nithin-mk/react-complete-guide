3f497e08e3ab00e0e89d822ec69b763c9cf4cd14
Nithin M.K <nithinmkurien@gmail.com>
2022-09-02 13:00:49 +0530

    06-bonus-multiple-slices

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import Navigation from './components/Nav/Navigation';
 import ProductsPage from './containers/Products';
 import FavoritesPage from './containers/Favorites';
+import Counter from './containers/Counter';
 
 const App = props => {
   return (
@@ -12,6 +13,7 @@ const App = props => {
       <main>
         <Route path="/" component={ProductsPage} exact />
         <Route path="/favorites" component={FavoritesPage} />
+        <Counter />
       </main>
     </React.Fragment>
   );
--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureProductsStore from './hooks-store/products-store';
+import configureCounterStore from './hooks-store/counter-store';
 
 configureProductsStore();
+configureCounterStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
