38f09d8e4572b5bf704c6b78ddeba2ee17a4837e
Nithin M.K <nithinmkurien@gmail.com>
2022-10-07 10:54:42 +0530

    07-working-with-mocks

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,7 +3,11 @@ import Async from './Async';
 
 describe('Async component', () => {
   test('renders posts if request succeeds', async () => {
-    render(<Async />)
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }],
+    });
+    render(<Async />);
 
     const listItemElements = await screen.findAllByRole('listitem');
     expect(listItemElements).not.toHaveLength(0);
