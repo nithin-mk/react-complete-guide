b761ac2e963a1b3e7347576df05ee36a19ebe1fd
Nithin M.K <nithinmkurien@gmail.com>
2022-10-08 23:03:09 +0530

    06-understanding-generics

--- basics.ts
+++ basics.ts
@@ -31,7 +31,7 @@ let person: Person;
 
 person = {
   name: 'Max',
-  age: 32
+  age: 32,
 };
 
 // person = {
@@ -46,3 +46,26 @@ let course: string | number = 'React - The Complete Guide';
 
 course = 12341;
 
+// Functions & types
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function print(value: any) {
+  console.log(value);
+}
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd')
+
+// updatedArray[0].split('');
